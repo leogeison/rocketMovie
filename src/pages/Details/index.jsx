9e42e77e7ecf12a9ffeb6eb346b66b6844796a7c
Léo Geison <leogeisom@gmail.com>
2023-06-14 18:29:42 -0300

    adding button component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,16 @@
 import { Container }  from "./styles"
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
       <h1>Testando aplicação</h1>
       <span>Léo Geison</span>
+
+      <Button title="Entrar" loading />
+      <Button title="Salvar"/>
+      <Button title="Cadastrar"/>
+      
     </Container>
   );
 }
