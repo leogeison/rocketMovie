375ce98f87a42dda4253ed86e7f7e99bdc240057
Léo Geison <leogeisom@gmail.com>
2023-06-21 20:45:21 -0300

    Implementing the ButtonText component

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.button`
+  background: none;
+  color: ${({ theme }) => theme.COLORS.SALMON};
+
+  border: none;
+  font-size: 16px;
+`;
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container }  from "./styles"
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
 
@@ -9,6 +10,7 @@ export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Voltar"/>
       <Section title="Marcadores">
         <Tag title="drama"/>
         <Tag title="comédia"/>
