a903fa1a7824fafe9d6a93a22d84a3a23f3ffc9f
Léo Geison <leogeisom@gmail.com>
2023-08-07 21:08:39 -0300

    application route files and authentication created

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Details } from './pages/Details/index.jsx';
 import { ThemeProvider } from 'styled-components';
 import Globalstyle from './styles/global.js';
-
 import theme from './styles/theme.js';
+
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Globalstyle />
-      <Details />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 );
