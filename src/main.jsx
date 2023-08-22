3e681e4b4a26154371d998ab6b2146c7018c196c
Léo Geison <leogeisom@gmail.com>
2023-08-22 19:44:39 -0300

    creation of signIn authentication context

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import theme from './styles/theme.js';
 
 import { Routes } from './routes';
 
+import { AuthProvider } from './hooks/auth';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Globalstyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
